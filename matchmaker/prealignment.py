82134ecfd85a4f09a70d461365dc796a7fafb4fe
Marei Freitag <marei.freitag@stud.uni-goettingen.de>
2025-08-06 10:53:17 +0200

    prealingment before updating overall structure

--- matchmaker/prealignment.py
+++ matchmaker/prealignment.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import logging
 import os
 import click
+import sys
+import napari
 
 from matchmaker.data import create_point_cloud
 from matchmaker.mobie_export import export_to_mobie
@@ -29,10 +31,10 @@ def get_SVD_transform(img, save_path=None):
     logging.info(f"Point cloud shape {pos.shape}")
     logging.info(f"Point cloud center {gc}")
 
-    logging.info("Run SVD")
+    logging.info("Run SVD ...")
     U, S, Vt = np.linalg.svd(pos_c, full_matrices=False)
     logging.info("U")
-    logging.info(U)
+    logging.info(str(U))
     logging.info("S")
     logging.info(str(S))
     logging.info("Vt")
@@ -41,6 +43,7 @@ def get_SVD_transform(img, save_path=None):
     logging.info("Rotate point cloud")
     vr = pos_c @ Vt.T
 
+    # TODO: update axis labels
     plt.figure(figsize=(10, 5))
     plt.subplot(1, 2, 1)
     plt.title("original vertices")
@@ -54,67 +57,53 @@ def get_SVD_transform(img, save_path=None):
     return gc, Vt
 
 
-def orient_yaxis(img, save_path=None):
-    """Euristic to orient all samples "head up": calculate sum intensity profile along the Y axis,
-    if max is closer to 0 then do nothing, else rotate 180 degrees.
-
-    Args:
-        img: DAPI volume
-
-    Returns:
-        True if rotation is needed.
-    """
+def orient_axis(img, axis, save_path=None):
+    logging.info(f"Orient along axis {axis}")
     assert img.ndim == 3, f"Input image should have 3 dimensions, has {img.ndim}"
-    int_profile = np.sum(img, axis=(0, 2))  # profile along y (2nd dimension)
+    if axis == 0:
+        int_profile = np.sum(img, axis=(1, 2))  # profile along z
+        R_3x3 = np.array([[1, 0, 0], [0, -1, 0], [0, 0, -1]])
+    if axis == 1:
+        int_profile = np.sum(img, axis=(0, 2))  # profile along y
+        R_3x3 = np.array([[-1, 0, 0], [0, 1, 0], [0, 0, -1]])
+    if axis == 2:
+        int_profile = np.sum(img, axis=(0, 1))  # profile along x
+        R_3x3 = np.array([[1, 0, 0], [0, -1, 0], [0, 0, 1]])
 
     plt.figure()
     plt.plot(int_profile)
     plt.xlabel("Y Coordinate")
-    plt.ylabel("Sum intensity along Y axis")
+    plt.ylabel(f"Sum intensity along axis = {axis}")
     if save_path is not None:
         plt.savefig(save_path, dpi=300)
     else:
         plt.show()
 
     max_pos = int_profile.argmax()
-    logging.info(f"Max position is {max_pos}, dimension shape is {img.shape[1]}")
+    logging.info(f"Max position is {max_pos}, dimension shape is {img.shape[axis]}")
     if max_pos < img.shape[1] // 2:
-        logging.info("Correct head orientation")
+        logging.info("Correct orientation")
         return False
     else:
-        logging.info("Rotate 180 degree to align head position")
+        logging.info("Rotate 180 degree to align")
         return True
 
 
-def orient_xaxis(img, save_path=None):
-    """Heuristic to orient all samples 'head left': calculate sum intensity profile along the X axis.
-    If max is closer to 0, then do nothing, else flip along x (mirror).
+# def orient_sample(img, axis, save_path=None):
+#     R_3x3 = orient_axis(img, axis=axis)  #, save_path=save_path)
+#     if R_3x3 is not None:
+#         img_center = 0.5 * (np.array(img_rotated.shape)-1)
+#         offset = img_center - R_3x3 @ img_center
+#         R = np.eye(4)
+#         R[:3, :3] = R_3x3
+#         R[:3, 3] = offset
+#         img_rotated = rotate_img(img_rotated, R, output_shape=img_rotated.shape)
+
+#         # update transformation matrix
+#         T = T @ R
+#         np.savetxt(f"{save_path}/{file_name}_T_prealignment.txt", T)
     
-    Returns:
-        True if mirroring is needed.
-    """
-    assert img.ndim == 3, f"Input image should have 3 dimensions, has {img.ndim}"
-    int_profile = np.sum(img, axis=(0, 1))  # profile along x (3rd dimension)
-
-    plt.figure()
-    plt.plot(int_profile)
-    plt.xlabel("X coordinate")
-    plt.ylabel("Sum intensity along X axis")
-    if save_path is not None:
-        plt.savefig(save_path, dpi=300)
-    else:
-        plt.show()
-
-    max_pos = int_profile.argmax()
-    logging.info(f"[orient_left] Max position is {max_pos}, shape[2] = {img.shape[2]}")
-    if max_pos < img.shape[2] // 2:
-        logging.info("Correct left-right orientation")
-        return False
-    else:
-        logging.info("Mirror along x-axis to align left-right")
-        return True
-
-# NOTE: also needed for z axis?
+#         return img_rotated
 
 
 def prealign_sample(img, file_name, save_path):
@@ -133,31 +122,10 @@ def prealign_sample(img, file_name, save_path):
     T, new_shape = get_transformation_matrix(img, gc, Vt, save_path=f"{save_path}/{file_name}_T_prealignment.txt")
     img_rotated = rotate_img(img, T, output_shape=new_shape)
 
-    # Check if head is oriented correctly, else rotate 180 degrees
-    rotate_y = orient_yaxis(
-        img_rotated, f"{save_path}/plots/{file_name}_intensity_profile_y.png"
-    )
-    if rotate_y:
-        print("Rotate head 180 degrees/ mirror on y axis ...")
-        R_3x3 = np.array([[-1, 0, 0], [0, -1, 0], [0, 0, 1]])
-        img_center = 0.5 * (np.array(img_rotated.shape)-1)
-        offset = img_center - R_3x3 @ img_center
-        R = np.eye(4)
-        R[:3, :3] = R_3x3
-        R[:3, 3] = offset
-        img_rotated = rotate_img(img_rotated, R, output_shape=img_rotated.shape)
-
-        # update transformation matrix
-        T = T @ R
-        np.savetxt(f"{save_path}/{file_name}_T_prealignment.txt", T)
-
-    # Check if left-right is oriented correctly, else mirror on x axis
-    rotate_x = orient_xaxis(
-        img_rotated, f"{save_path}/plots/{file_name}_intensity_profile_x.png"
-    )
-    if rotate_x:
-        print("Mirror on x axis ...")
+    if np.linalg.det(Vt.T) < 0:
+        print("WARNING: V includes a reflection (mirroring)")
         R_3x3 = np.array([[1, 0, 0], [0, 1, 0], [0, 0, -1]])
+        logging.info("Mirror back...")
         img_center = 0.5 * (np.array(img_rotated.shape)-1)
         offset = img_center - R_3x3 @ img_center
         R = np.eye(4)
@@ -168,6 +136,22 @@ def prealign_sample(img, file_name, save_path):
         # update transformation matrix
         T = T @ R
         np.savetxt(f"{save_path}/{file_name}_T_prealignment.txt", T)
+
+    # check orientation in every three dimensions/ if heads up
+    # for axis in [0, 1, 2]:
+    #     R_3x3 = orient_axis(img_rotated, axis=axis, save_path=f"{save_path}/plots/{file_name}_intensity_profile_{axis}.png")
+        # if R_3x3 is not None:
+        #     logging.info(f"Rotate axis {axis} 180 degrees to align...")
+        #     img_center = 0.5 * (np.array(img_rotated.shape)-1)
+        #     offset = img_center - R_3x3 @ img_center
+        #     R = np.eye(4)
+        #     R[:3, :3] = R_3x3
+        #     R[:3, 3] = offset
+        #     img_rotated = rotate_img(img_rotated, R, output_shape=img_rotated.shape)
+
+        #     # update transformation matrix
+        #     T = T @ R
+        #     np.savetxt(f"{save_path}/{file_name}_T_prealignment.txt", T)
 
     plot_three_slices(
         img_rotated,
@@ -178,20 +162,17 @@ def prealign_sample(img, file_name, save_path):
 
 
 def prealignment_per_image(input_path, input_key, output_dir, mobie_export, image_type):
-    # load input image
+    logging.info("Read image file")
     img = read_volume(input_path, input_key)
     file_name = os.path.splitext(os.path.basename(input_path))[0]
-
-    if not os.path.exists(f"{output_dir}/plots"):
-        os.makedirs(f"{output_dir}/plots")
 
     # plot three slices of input image
     plot_three_slices(img, save_path=f"{output_dir}/plots/{image_type}.png")
 
-    # pre-align image
+    logging.info("Prealign image")
     img_prealigned = prealign_sample(img, file_name, save_path=output_dir)
 
-    # save pre-aligned image
+    logging.info("Save prealigned image")
     attributes = dict(get_attrs(input_path, input_key))
     write_volume(
         f=f"{output_dir}/{file_name}_prealigned.n5",
@@ -202,6 +183,7 @@ def prealignment_per_image(input_path, input_key, output_dir, mobie_export, imag
 
     # export pre-aligned image to MoBIE
     if mobie_export:
+        logging.info("Export prealigned image to MoBIE")
         export_to_mobie(
             input_path=f"{output_dir}/{file_name}_prealigned.n5",
             input_key=input_key,
@@ -221,7 +203,19 @@ def run_prealignment(
     output_dir,
     mobie_export=True,
 ):
+    if not os.path.exists(f"{output_dir}/plots"):
+        os.makedirs(f"{output_dir}/plots")
 
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        handlers=[
+            logging.FileHandler(f"{output_dir}/prealignment.log", mode="w"),
+            logging.StreamHandler(sys.stdout),
+        ],
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+    logging.info("Start prealignment of fixed image ...")
     fixed_prealigned = prealignment_per_image(
         fixed_input_path,
         fixed_input_key,
@@ -230,6 +224,7 @@ def run_prealignment(
         image_type="fixed"
     )
 
+    logging.info("Start prealignment of moving image ...")
     moving_prealigned = prealignment_per_image(
         moving_input_path,
         moving_input_key,
@@ -237,15 +232,41 @@ def run_prealignment(
         mobie_export,
         image_type="moving",
     )
-    print(fixed_prealigned.dtype)
+    # check orientation
+    for axis in range(3):
+        rotate_axis_fixed = orient_axis(moving_prealigned, axis=axis, save_path=f"{output_dir}/plots/moving_intensity_profile_{axis}.png")
+        rotate_axis_moving = orient_axis(fixed_prealigned, axis=axis, save_path=f"{output_dir}/plots/fixed_intensity_profile_{axis}.png")
+        
+        if not rotate_axis_fixed and not rotate_axis_moving:
+            print(f"Correct orientation in axis {axis}")
+        else:
+            print(f"Rotate axis {axis} 180 degrees to align...")
+    R_3x3 = np.array([[-1, 0, 0], [0, -1, 0], [0, 0, 1]])
+    logging.info(f"Rotate axis {axis} 180 degrees to align...")
+    img_center = 0.5 * (np.array(moving_prealigned.shape)-1)
+    offset = img_center - R_3x3 @ img_center
+    R = np.eye(4)
+    R[:3, :3] = R_3x3
+    R[:3, 3] = offset
+    moving_prealigned = rotate_img(moving_prealigned, R, output_shape=moving_prealigned.shape)
+
+    # update transformation matrix
+    # T = T @ R
+    # np.savetxt(f"{output_dir}/moving_T_prealignment.txt", T)
+    
+    logging.info("Prealignment done.")
+
+    v = napari.Viewer()
+    v.add_labels(fixed_prealigned)
+    v.add_labels(moving_prealigned)
+    napari.run()
+    breakpoint()
 
     plot_overlay(
         fixed_prealigned,
         moving_prealigned,
         save_path=f"{output_dir}/plots/overlay_prealignment.png",
     )
-
-    # return fixed_prealigned, moving_prealigned
 
 
 @click.command()
