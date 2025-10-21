5e921bd86f0b81bf656ac73cfd02be9bdf528cc6
zh320 <68283806+zh320@users.noreply.github.com>
2025-10-21 16:40:18 +0200

    add unit tests for raw_to_n5, prealignment, and rigid alignment

---
+++ matchmaker/utils/test_utils.py
@@ -0,0 +1,23 @@
+import os
+import yaml
+
+
+def load_test_config(config_path="examples/register_config_test.yaml"):
+    assert os.path.exists(config_path)
+
+    try:
+        with open(config_path, "r") as f:
+            config = yaml.safe_load(f)
+    except Exception as e:
+        print(f'Unable to load test config file due to: {e}')
+        raise e
+
+    return config
+
+
+def get_n5_path():
+    config = load_test_config()
+    fixed_path = f"{config['log_dir']}/{config['fixed_image']['output_name']}.n5"
+    moving_path = f"{config['log_dir']}/{config['moving_image']['output_name']}.n5"
+
+    return fixed_path, moving_path
