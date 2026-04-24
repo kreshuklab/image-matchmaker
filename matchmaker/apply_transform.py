b8b0b20cee0ebc3dabd9b34c55d76fd73a2fa5d6
zh320 <68283806+zh320@users.noreply.github.com>
2026-04-24 17:11:43 +0200

    support .tif input and make fixed/prealignment params optional in apply_transform

--- matchmaker/apply_transform.py
+++ matchmaker/apply_transform.py
@@ -2,6 +2,7 @@ import itk
 import click
 import logging
 import numpy as np
+import tifffile as tiff
 from pathlib import Path
 
 from utils import (setup_logging, read_volume, write_volume, get_attrs, rotate_img,
@@ -9,30 +10,61 @@ from utils import (setup_logging, read_volume, write_volume, get_attrs, rotate_i
                     plot_overlay,)
 
 
+def load_data(path, key=None):
+    if path.endswith(".n5"):
+        assert key
+        data = read_volume(path, key)
+    elif path.endswith((".tif", ".tiff")):
+        data = tiff.imread(path)
+        if data.ndim == 2:
+            data = data[None, ...]
+    else:
+        raise NotImplementedError
+
+    return data.astype(np.float32)
+
+
+def save_data(data, input_path, output_dir, output_key=None, **kwargs):
+    if input_path.endswith((".tif", ".tiff")):
+        output_path = f"{output_dir}/{Path(input_path).stem}_transformed.tif"
+        print("output_path", output_path)
+        tiff.imwrite(output_path, data)
+    elif input_path.endswith(".n5"):
+        assert output_key
+        write_volume(input_path, data, output_key, **kwargs)
+    else:
+        raise NotImplementedError
+
+
 @click.command()
-@click.option("-fi", "--fixed_path", required=True, help="Fixed input .n5 file")
-@click.option("-fk", "--fixed_key", required=True, help="Fixed input key")
+@click.option("-fi", "--fixed_path", default=None, help="Fixed input .n5 file")
+@click.option("-fk", "--fixed_key", default=None, help="Fixed input key")
 @click.option("-mi", "--moving_path", required=True, help="Moving input .n5 file")
 @click.option("-mk", "--moving_key", required=True, help="Moving input key")
+@click.option("-mr", "--moving_resolution", type=float, nargs=3, required=True, help="Moving resolution")
 @click.option("-o", "--output_dir", required=True, help="Output directory")
-@click.option("-ok", "--output_key", required=True, help="Output key for the regsitered moving image",)
+@click.option("-ok", "--output_key", default=None, help="Output key for the regsitered moving image",)
 @click.option("-pm", "--parameter_map_path", required=True, help="Path to the parameter map",)
-@click.option("-pt", "--prealignment_transform_path", required=True, help="Prealignment transform path",)
+@click.option("-pt", "--prealignment_transform_path", default=None, help="Prealignment transform path",)
 @click.option("-io", "--interpolation_order", default=0, help="Order of interpolation",)
-def apply_transform(fixed_path, fixed_key, moving_path, moving_key, output_dir, output_key,
-                    parameter_map_path, prealignment_transform_path, interpolation_order):
+def apply_transform(fixed_path, fixed_key, moving_path, moving_key, moving_resolution,
+                    output_dir, output_key, parameter_map_path, prealignment_transform_path,
+                    interpolation_order):
 
     log_dir = Path(output_dir)
     log_dir.mkdir(exist_ok=True)
 
     setup_logging(log_dir, "apply_transform.log")
 
-    logging.info("Read image file")
-    fixed_img_np = read_volume(fixed_path, fixed_key).astype(np.float32)
-    fixed_resolution = get_attrs(fixed_path, fixed_key)["resolution"]
+    if fixed_path:
+        logging.info("Read fixed image")
+        fixed_img_np = load_data(fixed_path, fixed_key)
 
-    moving_img_np = read_volume(moving_path, moving_key).astype(np.float32)
-    moving_resolution = get_attrs(moving_path, moving_key)["resolution"]
+    logging.info("Read moving image")
+    moving_img_np = load_data(moving_path, moving_key)
+    if moving_path.endswith(".n5"):
+        if list(moving_resolution) != get_attrs(moving_path, moving_key)["resolution"]:
+            raise ValueError("Moving resolution from config is different from n5 file")
 
     if moving_img_np.ndim == 3:
         moving_img_np = moving_img_np[None, ...]
@@ -54,34 +86,45 @@ def apply_transform(fixed_path, fixed_key, moving_path, moving_key, output_dir, 
 
     logging.info("Plot transformed image")
     plot_three_slices(result_img_np, save_path=log_dir / "deformable_pointset_transformed_moving.png")
-    plot_overlay(
-        fixed_img_np,
-        result_img_np,
-        log_dir / f"deformable_pointset_transformed_overlay.png",
-    )
-
-    if prealignment_transform_path is not None:
-        logging.info("Rotate image using prealignment transform")
-        prealignment_transform = read_transform_dict(prealignment_transform_path)["fixed_prealignment"]
-        T_fixed, output_shape = prealignment_transform["matrix"], prealignment_transform["output_shape"]
-
-        fixed_img_np = rotate_img(fixed_img_np, T_fixed, output_shape=output_shape)
-        result_img_np = rotate_img(result_img_np, T_fixed, output_shape=output_shape)
-
-        logging.info("Plot transformed images after pre-alignment")
-        plot_three_slices(result_img_np, save_path=log_dir / "deformable_pointset_transformed_moving_prealigned.png")
+    if fixed_path:
+        logging.info("Plot overlay image")
         plot_overlay(
             fixed_img_np,
             result_img_np,
-            log_dir / f"deformable_pointset_transformed_prealigned_overlay.png",
+            log_dir / f"deformable_pointset_transformed_overlay.png",
         )
 
-    logging.info("Write results")
-    attributes = dict(get_attrs(moving_path, moving_key))
-    resolution = [float(res) for res in parameter_object.GetParameter(0, "Spacing")]
-    attributes["resolution"] = resolution
+    if prealignment_transform_path:
+        logging.info("Rotate moving image using prealignment transform")
+        prealignment_transform = read_transform_dict(prealignment_transform_path)["fixed_prealignment"]
+        T_fixed, output_shape = prealignment_transform["matrix"], prealignment_transform["output_shape"]
+        result_img_np = rotate_img(result_img_np, T_fixed, output_shape=output_shape)
 
-    write_volume(moving_path, result_img_np, output_key, chunks=chunks, attrs=attributes)
+        logging.info("Plot transformed moving images after pre-alignment")
+        plot_three_slices(result_img_np, save_path=log_dir / "deformable_pointset_transformed_moving_prealigned.png")
+
+        if fixed_path:
+            logging.info("Rotate fixed image using prealignment transform")
+            fixed_img_np = rotate_img(fixed_img_np, T_fixed, output_shape=output_shape)
+
+            logging.info("Plot overlay images after pre-alignment")
+            plot_overlay(
+                fixed_img_np,
+                result_img_np,
+                log_dir / f"deformable_pointset_transformed_prealigned_overlay.png",
+            )
+
+    logging.info("Write results")
+    resolution = [float(res) for res in parameter_object.GetParameter(0, "Spacing")]
+
+    save_attrs = {}
+    if moving_path.endswith(".n5"):
+        attributes = dict(get_attrs(moving_path, moving_key))
+        attributes["resolution"] = resolution
+        save_attrs["chunks"] = chunks
+        save_attrs["attrs"] = attributes
+
+    save_data(result_img_np, moving_path, output_dir, output_key=output_key, **save_attrs)
 
 
 if __name__ == "__main__":
