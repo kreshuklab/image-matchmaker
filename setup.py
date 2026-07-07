e7f59eda4ea417dfb5b20998a0282fc4c043718b
Marei Freitag <marei.freitag@stud.uni-goettingen.de>
2026-07-07 14:29:27 +0200

    create version file and update requirements in setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,39 @@
+import runpy
 from setuptools import setup, find_packages
+
+__version__ = runpy.run_path('matchmaker/__version__.py')['__version__']
+
+requires = [
+    'click',
+    'cvxpy',
+    'itk-elastix',
+    'matplotlib',
+    'numpy',
+    'open3d',
+    'pandas',
+    'probreg',
+    'python-elf',
+    'pyyaml',
+    'scikit-image',
+    'scipy',
+    'seaborn',
+    'tifffile',
+    'transforms3d',
+    'z5py',
+]
+
+# NOTE: `mobie_utils` (imported as `mobie` in matchmaker/mobie_export.py) has no
+# PyPI release; install it via conda when using the mobie export functionality:
+#   conda install -c conda-forge mobie_utils
 
 setup(
     name='matchmaker',
-    version='0.1',
+    version=__version__,
     description='Registration of instance segmentations .',
     url='https://github.com/kreshuklab/matchmaker/',
     packages=find_packages(include=['matchmaker']),
     python_requires='>=3.6',
-    install_requires=[],
+    install_requires=requires,
     author='Elena Buglakova',
     author_email='elena.buglakova@embl.de',
     license='MIT'
