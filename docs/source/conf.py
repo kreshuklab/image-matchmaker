13c0574dd47dd2851c8bac72e3e5efeb2948b770
Marei Freitag <marei.freitag@stud.uni-goettingen.de>
2026-07-13 17:03:08 +0200

    also rename project to image_matchmaker

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'bio_matchmaker'
+project = 'image_matchmaker'
 copyright = '2026, kreshuklab'
 author = 'kreshuklab'
 release = '1.0'
