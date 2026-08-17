072f6a8668997f287aabb98792f9aeaa25a08147
Marei Freitag <marei.freitag@stud.uni-goettingen.de>
2026-08-17 13:37:50 +0200

    add init file for cpd optimization

--- setup.py
+++ setup.py
@@ -5,7 +5,12 @@ setup(
     version='0.1',
     description='Registration of instance segmentations .',
     url='https://github.com/kreshuklab/image-matchmaker',
-    packages=find_packages(include=['image_matchmaker']),
+    packages=find_packages(include=['image_matchmaker', 'image_matchmaker.*']),
+    # Loaded at runtime relative to __file__, so they have to ship with the package
+    package_data={
+        'image_matchmaker': ['*.txt'],
+        'image_matchmaker.cpd_parameter_tuning': ['*.yaml'],
+    },
     python_requires='>=3.6',
     install_requires=[],
     author='Elena Buglakova',
